577b13cb474a5ca52f9b345182eea0d51f440d68
Joey <jvalencia002@hotmail.com>
2016-03-23 21:58:11 -0700

    Added files via upload

---
+++ Arrays.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrays;
+
+/**
+ *
+ * @author Joeyvalencia
+ */
+public class Arrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] lottery_number = new int[49];
+        int i;
+        
+        for (i=0; i < lottery_numbers.length; i++){
+            lottery_number[i] = i + 1;
+            System.out.println(lottery_numbers[i]);
+    }
+    
+}
+}
